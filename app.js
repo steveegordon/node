9c164177351a7830432bee6e0c4c8c43834f8203
stevegordon <tdtsgordon@gmail.com>
2017-05-23 10:05:40 -0700

    use node emitter and create config file to avoid magic string errors

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var greet = require('./greet');
+var eventConfig = require('./config').events;
+//  Actual node emitters
+var Emitter = require('events');
 // References same cached function
 // In order to access different variables use constructor in app.js
 var greet2 = require('./greet');
@@ -7,3 +10,16 @@ var math = require('./math');
 greet();
 math.add(2, 5);
 math.subtract(10, 5);
+
+var emtr = new Emitter();
+
+emtr.on(eventConfig.GREET, function() {
+  console.log('Somewhere, someone said hello.');
+});
+
+emtr.on(eventConfig.GREET, function() {
+  console.log('A greeting occurred!');
+});
+
+console.log('Hello!');
+emtr.emit(eventConfig.GREET);
