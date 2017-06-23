8312ff34c28c261100b3fe0b79d980e332708729
stevegordon <tdtsgordon@gmail.com>
2017-06-23 12:51:06 -0700

    create and use readable and writeable streams

--- app.js
+++ app.js
@@ -1,107 +1,13 @@
 'use strict';
-
-// var greet = require('./greet');
-var eventConfig = require('./config').events;
-//  Actual node emitters
-var Emitter = require('events');
-// util.js adds extra functionality
-var util = require('util');
-// References same cached function
-// In order to access different variables use constructor in app.js
-// var greet2 = require('./greet');
-// var math = require('./math');
-
-// greet();
-// math.add(2, 5);
-// math.subtract(10, 5);
-
-// var emtr = new Emitter();
-
-// emtr.on(eventConfig.GREET, function() {
-//   console.log('Somewhere, someone said hello.');
-// });
-
-// emtr.on(eventConfig.GREET, function() {
-//   console.log('A greeting occurred!');
-// });
-
-// console.log('Hello!');
-// emtr.emit(eventConfig.GREET);
-// 
-
-// USE util to create events inheritance for Greetr
-class Greetr extends Emitter {
-  constructor() {
-    super();
-    this.greeting = 'Hello world!';
-  }
-  greet(data) {
-    console.log(` ${ this.greeting } : ${ data }`);
-    this.emit('greet', data);
-  }
-}
-
-// function Greetr() {
-//   // this .call properly binds variables and methods
-//   Emitter.call(this);
-//   this.greeting = 'Hello world!';
-// }
-
-
-var greeter1 = new Greetr();
-
-greeter1.on('greet', function(data) {
-  console.log('Someone greeted!: ' + data);
+// fs is used for file access
+var fs = require('fs');
+// create a readable stream that reads from initial.txt in 16kb chunks encoded in utf8
+var readable = fs.createReadStream(__dirname + '/initial.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
+// create a writable stream that writes to copy.txt
+var writable = fs.createWriteStream(__dirname + '/copy.txt');
+// stream event sends 16kb chunks
+readable.on('data', function(chunk) {
+  console.log(chunk.length);
+  // write 16kb chunks as they come using the writable string
+  writable.write(chunk);
 });
-// Passing data down the chain through the emitter
-greeter1.greet("Umberto");
-
-var name = "Steve";
-// Template literal string concat ES6 in node
-// avoid of use babeljs.io for front end for compatability
-var hello = `Hello, ${ name }`;
-console.log(hello);
-
-var obj = {
-  name: 'player1',
-  greet: function(age) {
-    console.log(`Hello ${ this.name }, your are ${ age } years old.`);
-  }
-};
-
-// apply and call allow you to use 'borrow' code
-// with different params
-obj.greet(12);
-obj.greet.call({name: 'player2'}, 15);
-obj.greet.apply({name: 'player3'}, [27]);
-
-// ES6 class similar to Ruby
-class Dog {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  bark() {
-    console.log(`Woof, Woof, my name is ${ this.name }`);
-  }
-}
-
-var jack = new Dog('Jack Gordon', 2);
-jack.bark();
-
-var buf = new Buffer('Hello', 'utf8');
-console.log(buf);
-console.log(buf.toString());
-console.log(buf.toJSON());
-// .write replaces buffer index's
-// wollo
-buf.write('wo');
-console.log(buf.toString());
-// ES6 buffer, not used in node however
-var buffer = new ArrayBuffer(8);
-var view = new Int32Array(buffer);
-view[1] = 12;
-view[0] = 0;
-console.log(view);
-
-
