1931d94d0cf561e47fa970aaa1164f4837c53ec5
stevegordon <tdtsgordon@gmail.com>
2017-06-27 23:27:24 -0700

    use pipes to chain and transform streams

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 'use strict';
 // fs is used for file access
 var fs = require('fs');
+// zlib offers the ability to compress files in node
+var zlib = require('zlib');
 // create a readable stream that reads from initial.txt in 16kb chunks encoded in utf8
-var readable = fs.createReadStream(__dirname + '/initial.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
+var readable = fs.createReadStream(__dirname + '/initial.txt');
 // create a writable stream that writes to copy.txt
 var writable = fs.createWriteStream(__dirname + '/copy.txt');
-// stream event sends 16kb chunks
-readable.on('data', function(chunk) {
-  console.log(chunk.length);
-  // write 16kb chunks as they come using the writable string
-  writable.write(chunk);
-});
+// write stream that writes to gzipped file
+var compressed = fs.createWriteStream(__dirname + '/initial.txt.gz');
+
+// creates a transform stream that is readable and writeable and compresses all chunks. can be piped.
+var gzip = zlib.createGzip();
+
+
+// Shorthand for piping. .pipe(writable) returns writeable destination so you can
+// chain pipes together, ala readable.pipe(readwritestream).pipe(writablestream)
+readable.pipe(writable);
+
+// readable stream pipes data to gzip which compresses chunks, compressed chunks
+// are then further piped and compressed data is placed in gzipped file.
+readable.pipe(gzip).pipe(compressed);
