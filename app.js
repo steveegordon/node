7ddee805720591525a6be0d3f8b8f89d3f8c5a93
stevegordon <tdtsgordon@gmail.com>
2017-07-01 17:49:58 -0700

    web server sending files with custom messages

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 'use strict';
 // Bring in http module
 var http = require('http');
+// Bring in filsync module
+var fs = require('fs');
 
 // create a server, tell it how to handle requests and responses
 http.createServer(function(req, res) {
 // set only one response for all request types, specify response ok/200
-// specify response in strictly plain text
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-// body is only the words hello world
-  res.end('Hello world\n');
+// specify response is html
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  // index.html as html, specify utf for modifying purposes
+  var html = fs.readFileSync(__dirname + '/index.html', 'utf8' );
+  // create a variable we are going to replace
+  var message = 'Hello world...';
+  // replace instances of {Message} in html with our custom message
+  html = html.replace( '{Message}', message);
+  // response sends our modified html file
+  res.end(html);
 
 // set up the port server is listening on
 }).listen(1337, '127.0.0.1');
