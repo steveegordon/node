c0d7790cad70a2a77a429a013a6b657ece2cec11
stevegordon <tdtsgordon@gmail.com>
2017-07-11 09:39:24 -0700

    use bodyParser module to access post data

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // bring in express module
 var express = require('express');
+// bring in bodyParser module for accessing post data
+var bodyParser = require('body-parser');
 // create app which has all express methods
 var app = express();
 // set a port using an environmental variable or set it to 1337
 var port = process.env.PORT || 1337;
+// required by bodyParser to create req.body object for accessing post data
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// required by bodyParser to create req.body object for accessing JSON post data
+var jsonParser = bodyParser.json();
 // middleware, when assets are referenced express looks to the public
 //  directory to load them
 app.use('/assets', express.static(__dirname + '/public'));
@@ -22,8 +28,24 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 // get id specific request, send back data specific to requests params
+// get requests search req.query.-value-
 app.get('/person/:id', function(req, res) {
-  res.render('person', { ID: req.params.id });
+  res.render('person', { ID: req.params.id, Qstr:
+  req.query.qstr });
+});
+// post request sent from '/' form to '/person' parsed and responded in node 
+// console, responds with '/person' url and basic text response message
+app.post('/person', urlencodedParser, function(req, res) {
+  res.send("Thank you!");
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+});
+// jquery ajax post sending JSON sent from '/' onload doesn't load '/personjson'
+// url but posts data to location no res shown
+app.post('/personjson', jsonParser, function(req, res) {
+  res.send('Thank you for the JSON data!');
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
 });
 // if get request to api endpoint respond with json
 app.get('/api', function(req, res) {
