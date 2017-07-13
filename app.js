dfc1cd058fbe29a4e4c80a00cb36ed6ff5014d48
stevegordon <tdtsgordon@gmail.com>
2017-07-13 10:19:53 -0700

    example of mysql usage no database so does not actually work

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 var express = require('express');
 // create app which has all express methods
 var app = express();
-//
+// bring in mysql module for javascript data handling
+var mysql = require('mysql');
+// controller contains html routes
 var htmlController = require('./controllers/htmlController');
-//
+// controller contains api routes
 var apiController = require('./controllers/apiController');
 // set a port using an environmental variable or set it to 1337
 var port = process.env.PORT || 1337;
@@ -17,16 +19,30 @@ app.set('view engine', 'ejs');
 // '/' and any of its sub-domains...other middleware includes cookie parsers etc
 app.use('/', function(req, res, next) {
   console.log('Request Url: ' + req.url);
+  // basic connection to mysql database
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "test",
+    password: "test",
+    database: "addressbook"
+  });
+  // example of querying an address book from a mysql database
+  con.query('SELECT people.ID, Firstname, Lastname, Address FROM People INNER JOIN PersonAddress ON People.ID = PersonAddresses.PersonID INNER JOIN Addresses ON PersonAddresses.AddressID = Addresses.ID',
+    // callback to the query responds with error if there is one, followed by the data
+    function(err, rows) {
+      if(err) throw err;
+      // rows will be an array of javascript objects, this example returns the
+      // first object, can access like typical javascript for example rows[0].firstname
+      console.log(rows[0]);
+    }
+  );
   // next is used to chain middleware, in this instance it allows the request to
   // continue which means the app looks how to handle the get request
   next();
 });
-
-// if get request to api endpoint respond with json
-// app.get('/api', function(req, res) {
-//   res.json({ firstname: 'Steve', lastname: 'Gordon'});
-// });
+// pass htmlControllers functions to app
 htmlController(app);
+// pass apiControllers functions to app
 apiController(app);
 // set app to listen to port variable
 app.listen(port);
