ba48b2286ffd265cf1e23328cd05d52a8d23ee16
stevegordon <tdtsgordon@gmail.com>
2017-08-17 16:03:57 -0700

    create seeding method

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ var mongoose = require('mongoose');
 
 var config = require('./config');
 
-var port = process.end.PORT || 3000;
+var setupController = require('./controllers/setupController');
+
+var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
+setupController(app);
 
 app.listen(port);
